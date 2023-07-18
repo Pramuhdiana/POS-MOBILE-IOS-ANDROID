7c7ca2aa21fa0b593e6225e0ba1cca69ba6e6b3d
pramuhdiana <sandypramuhdiana96@gmail.com>
2023-07-18 16:00:37 +0700

    setting appdelegate untuk notif ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,11 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
       //coba add code nanti di bawah
+      if #available(iOS 10.0, *) {
+          UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+         }
+        
+      application.registerForRemoteNotifications()
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
