38d2d30ca1f4cbf8549265b3169687bc03615c27
Pramuhdiana <sandypramuhdiana96@gmail.com>
2023-11-13 16:55:18 +0700

    fix bug search event

--- lib/search/new_search_event.dart
+++ lib/search/new_search_event.dart
@@ -2,21 +2,17 @@
 
 import 'dart:convert';
 
-import 'package:cached_network_image/cached_network_image.dart';
 // import 'package:badges/badges.dart' as badges;
-import 'package:collection/collection.dart';
 import 'package:e_shop/api/api_constant.dart';
 import 'package:e_shop/database/model_allitems.dart';
 // import 'package:e_shop/event/cart_event_screen.dart';
 import 'package:e_shop/event/search_pos_event.dart';
 // import 'package:e_shop/event/transaksi_event_screen.dart';
 import 'package:e_shop/global/global.dart';
-import 'package:e_shop/provider/provider_cart_event.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:lottie/lottie.dart';
-import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
 
 class NewSearchEventScreen extends StatefulWidget {
@@ -30,14 +26,15 @@ class _SearchScreenState extends State<NewSearchEventScreen> {
     final response = await http.get(
         Uri.parse(ApiConstants.baseUrl + ApiConstants.GETposTokoendpoint),
         headers: {"Authorization": "Bearer $tokens"});
-    print('status : ${response.statusCode}');
     if (response.statusCode == 200) {
       List jsonResponse = json.decode(response.body);
 
       var allData =
           jsonResponse.map((data) => ModelAllitems.fromJson(data)).toList();
-      var filterByLot =
-          allData.where((element) => element.name.toString().contains(lot));
+      var filterByIdToko = allData.where(
+          (element) => element.customer_id.toString().toLowerCase() == '440');
+      var filterByLot = filterByIdToko
+          .where((element) => element.name.toString().contains(lot));
       allData = filterByLot.toList();
 
       return allData;
@@ -76,50 +73,6 @@ class _SearchScreenState extends State<NewSearchEventScreen> {
             });
           },
         ),
-        // actions: [
-        //   Padding(
-        //     padding: const EdgeInsets.only(right: 10),
-        //     child: Stack(
-        //       children: [
-        //         IconButton(
-        //           onPressed: () {
-        //             Navigator.push(
-        //                 context,
-        //                 MaterialPageRoute(
-        //                     builder: (c) => const CartEventScreen()));
-        //             // }
-        //           },
-        //           icon: Padding(
-        //             padding: const EdgeInsets.all(2),
-        //             child: badges.Badge(
-        //               showBadge: context.read<PCartEvent>().getItems.isEmpty
-        //                   ? false
-        //                   : true,
-        //               badgeStyle: const badges.BadgeStyle(
-        //                 badgeColor: Colors.green,
-        //               ),
-        //               badgeContent: Text(
-        //                 context.watch<PCartEvent>().getItems.length.toString(),
-        //                 style: const TextStyle(
-        //                   fontSize: 8,
-        //                   fontWeight: FontWeight.w600,
-        //                 ),
-        //               ),
-        //               child: Transform.scale(
-        //                 scale: 1.3,
-        //                 child: Image.asset(
-        //                   "assets/cart.png",
-        //                   width: 45,
-        //                   height: 45,
-        //                 ),
-        //               ),
-        //             ),
-        //           ),
-        //         ),
-        //       ],
-        //     ),
-        //   ),
-        // ],
       ),
       body: searchInput == ''
           ? Center(
@@ -191,191 +144,3 @@ class _SearchScreenState extends State<NewSearchEventScreen> {
     );
   }
 }
-
-class SearchModel extends StatelessWidget {
-  final dynamic e;
-  const SearchModel({Key? key, required this.e}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      // onTap: () {
-      // Navigator.push(
-      //     context,
-      //     MaterialPageRoute(
-      //         builder: (context) => ProductDetailsScreen(proList: e)));
-      // Fluttertoast.showToast(msg: "Not Available");
-      // Navigator.push(
-      //     context,
-      //     MaterialPageRoute(
-      //         builder: (c) => ItemsDetailsScreen(
-      //               model: e.model,
-      //             )));
-      // },
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-        child: Container(
-          decoration: BoxDecoration(
-              color: Colors.white, borderRadius: BorderRadius.circular(12)),
-          height: 100,
-          width: double.infinity,
-          child: Padding(
-            padding: const EdgeInsets.only(right: 10),
-            child: Row(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(8),
-                  child: SizedBox(
-                    height: 100,
-                    width: 100,
-                    child: CachedNetworkImage(
-                      imageUrl:
-                          'https://parvabisnis.id/uploads/products/${e['image_name'].toString()}',
-                      placeholder: (context, url) => Center(
-                          child: Container(
-                              padding: const EdgeInsets.all(0),
-                              width: 90,
-                              height: 90,
-                              child: Lottie.asset("json/loading_black.json"))),
-                      errorWidget: (context, url, error) => Image.asset(
-                        "images/noimage.png",
-                      ),
-                      height: 124,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                Flexible(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Row(
-                        children: [
-                          Text(
-                            e['name'],
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 1,
-                            style: const TextStyle(
-                                fontSize: 14,
-                                color: Colors.black,
-                                fontWeight: FontWeight.w600),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Text(
-                            '\$ ${e['price']}',
-                            overflow: TextOverflow.ellipsis,
-                            // maxLines: 2,
-                            style: const TextStyle(
-                              fontSize: 15,
-                              color: Colors.red,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                IconButton(
-                  onPressed: () async {
-                    var existingitemcart = context
-                        .read<PCartEvent>()
-                        .getItems
-                        .firstWhereOrNull(
-                            (element) => element.name == e['name'].toString());
-                    //cart API
-                    print(existingitemcart);
-                    // existingitemcart == null
-                    if (existingitemcart == null) {
-                      String token =
-                          sharedPreferences!.getString("token").toString();
-                      //add cart API
-                      Map<String, String> body = {
-                        'product_id': e['id'].toString(),
-                        'qty': '1',
-                        'price': e['price'].toString(),
-                        'customer_id': '440',
-                        'jenisform_id': '2',
-                        'update_by': '1'
-                      };
-                      final response = await http.post(
-                          Uri.parse(ApiConstants.baseUrl +
-                              ApiConstants.POSTkeranjangtokoendpoint),
-                          headers: <String, String>{
-                            'Authorization': 'Bearer $token',
-                          },
-                          body: body);
-                      print(response.body);
-
-                      Fluttertoast.showToast(
-                          msg: "Barang Berhasil Di Tambahkan");
-                      context.read<PCartEvent>().addItem(
-                            e['name'].toString(),
-                            int.parse(e['price'].toString()),
-                            1,
-                            e['image_name'].toString(),
-                            e['id'].toString(),
-                            e['sales_id'].toString(),
-                            e['description'].toString(),
-                            e['keterangan_barang'].toString(),
-                          );
-                    } else {
-                      Fluttertoast.showToast(
-                          msg: "Barang Sudah Ada Di Keranjang");
-                    }
-                  },
-                  hoverColor: Colors.green,
-                  icon: const Icon(
-                    Icons.shopping_cart,
-                    color: Colors.black,
-                  ),
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-
-//stream buildnder with firebase
-// StreamBuilder<QuerySnapshot>(
-//               stream: FirebaseFirestore.instance
-//                   // .collection('items')
-//                   // .where('salesUID',
-//                   //     isEqualTo: sharedPreferences!.getString("uid")!)
-//                   // .where('posisi_id', isEqualTo: '1')
-//                   // .snapshots(),
-//                   .collection('allitems')
-//                   .where('sales_id', isEqualTo: int.parse(id!)) //sales id
-//                   .where('posisi_id', isEqualTo: 3)
-//                   .snapshots(),
-//               builder: (BuildContext context,
-//                   AsyncSnapshot<QuerySnapshot> snapshot) {
-//                 if (snapshot.connectionState == ConnectionState.waiting) {
-//                   return const Material(
-//                     child: Center(
-//                       child: CircularProgressIndicator(),
-//                     ),
-//                   );
-//                 }
-//                 final result = snapshot.data!.docs.where(
-//                   (e) => e['name'.toLowerCase()]
-//                       .contains(searchInput.toLowerCase()),
-//                 );
-//                 return ListView(
-//                   children: result.map((e) => SearchModel(e: e)).toList(),
-//                 );
-//               }),
-
-
