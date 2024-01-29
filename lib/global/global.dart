f0c834b823a234512e763e18ccab39736a32abc5
Pramuhdiana <sandypramuhdiana96@gmail.com>
2024-01-29 08:33:30 +0700

    version 27

--- lib/global/global.dart
+++ lib/global/global.dart
@@ -20,6 +20,6 @@ int revisiKecil =
     1; //perubahan kecil                                    (MINOR)
 int rilisPerbaikanbug =
     0; //perbaikan bug                                      (PATCH)
-int noBuild = 26; //nobuild                                      (PATCH)
+int noBuild = 27; //nobuild                                      (PATCH)
 
 String version = 'v$revisiBesar.$revisiKecil.$rilisPerbaikanbug ($noBuild)';
